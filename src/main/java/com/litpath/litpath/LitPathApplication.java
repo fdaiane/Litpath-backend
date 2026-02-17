b04c03e07c42397f10d6a44e5378c62fbf04d3af
Daiane <franciscasampaio284@gmail.com>
2026-02-17 20:05:33 -0300

    first commit

---
+++ src/main/java/com/litpath/litpath/LitPathApplication.java
@@ -0,0 +1,15 @@
+package com.litpath.litpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitPathApplication.class, args);
+	}
+
+}
+
+//./mvnw spring-boot:run
---
+++ src/test/java/com/litpath/litpath/LitPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litpath.litpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
