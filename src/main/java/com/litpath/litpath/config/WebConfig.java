34ecadfefe0e186ff929819181eb3ee32d1ec931
Daiane <franciscasampaio284@gmail.com>
2026-03-22 23:38:39 -0300

    alteracao config

--- src/main/java/com/litpath/litpath/config/WebConfig.java
+++ src/main/java/com/litpath/litpath/config/WebConfig.java
@@ -14,9 +14,13 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                            "http://localhost:5173",
+                            "https://lit-path-frontend.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
